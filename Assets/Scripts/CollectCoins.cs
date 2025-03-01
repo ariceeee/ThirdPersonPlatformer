848ffbb05b13f97881da4ff8b2428d751051b4ea
Ariana Rice <69221845+ariceeee@users.noreply.github.com>
2025-02-28 22:08:11 -0800

    Coin count is now updated visually in UI

--- Assets/Scripts/CollectCoins.cs
+++ Assets/Scripts/CollectCoins.cs
@@ -1,14 +1,23 @@
+using TMPro;
 using UnityEngine;
 
 public class CollectCoins : MonoBehaviour
 {
-    private int numCoins = 0;
+    private int numCoins;
+    [SerializeField] private TextMeshProUGUI coinText;
+
+    void Start()
+    {
+        numCoins = 0;
+        coinText.text = "Coins: " + numCoins;
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
             numCoins++;
+            coinText.text = "Coins: " + numCoins;
             Debug.Log("Player collected a coin! Total coins: " + numCoins);
             Destroy(other.gameObject);
         }
